b54f0c862a9250ac400c5d8234485dc0265ae39e
jynt1401 <jayantbelwanshi1401@gmail.com>
2023-04-14 00:22:50 +0530

    navigate progmatically

--- hello_world/pages/index.jsx
+++ hello_world/pages/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import Link from 'Next/Link'
+import React from "react";
+import Link from "Next/Link";
+import { useRouter } from "next/router";
 
 export default function index() {
+  const router=useRouter()
+  const handleClick=()=>{
+    console.log("place your order")
+    router.push('/product')
+    // router.replace('/product')
+  }
   return (
     <div>
       this is home page
-
       <div>
-        <Link href='/blog'>
-        blog
-        </Link>
+        <Link href="/blog">blog</Link>
       </div>
       <div>
-        <Link href='/product'>
-        product
-        </Link>
+        <Link href="/product">product</Link>
+      </div>
+      <div>
+        <button onClick={handleClick}> 
+          place a order
+        </button>
       </div>
     </div>
-  )
+  );
 }
