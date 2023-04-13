d9a847af1abba28833f9a182890d2a8751de7a89
jynt1401 <jayantbelwanshi1401@gmail.com>
2023-04-13 23:55:26 +0530

    catch all routes

---
+++ hello_world/pages/doc/[[...param]].jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+
+export default function doc() {
+    const {param=[]}=router.query
+    console.log(param)
+ 
+    if(param.length===2){
+        return(
+            <div>
+
+            <h1>doc for {param[0]}</h1>
+            <h1>doc doc for {param[1]}</h1>
+            </div>
+        )
+    }
+    else if(param.length===1){
+        return(
+            <h1>{param[0]}</h1>
+        )
+    }
+    return(
+        <h1>home doc page</h1>
+    )
+}
