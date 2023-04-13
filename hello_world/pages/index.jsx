71d00dca3e374dfd5269df481b00d2e2d87570bb
jynt1401 <jayantbelwanshi1401@gmail.com>
2023-04-14 00:13:11 +0530

    link

--- hello_world/pages/index.jsx
+++ hello_world/pages/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Link from 'Next/Link'
 
 export default function index() {
   return (
     <div>
       this is home page
+
+      <div>
+        <Link href='/blog'>
+        blog
+        </Link>
+      </div>
+      <div>
+        <Link href='/product'>
+        product
+        </Link>
+      </div>
     </div>
   )
 }
--- hello_world/pages/product/index.jsx
+++ hello_world/pages/product/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import Link from 'Next/Link'
 
 export default function index() {
   return (
     <div>
       <h1>product list</h1>
-      <h3>product 1</h3>
-      <h3>product 2</h3>
-      <h3>product 3</h3>
-      <h3>product 4</h3>
+      <div>
+        <Link href='/product/1'>
+        p1
+        </Link>
+      </div>
+      <div>
+        <Link href='/product/2'>
+        p2
+        </Link>
+      </div>
+      <div>
+        <Link href='/product/3'>
+        p3
+        </Link>
+      </div>
     </div>
   )
 }
